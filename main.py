b79e487b6e00ac58beeca678660a29d6ccf0315d
adamchant604 <adamchant604@gmail.com>
2024-06-05 21:03:15 -0400

    updated main.py char_count() and created sort_dict()

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ def char_count(string):
     lower_string = string.lower()
     chars_dict = {}
     for char in lower_string:
-        if char not in chars_dict:
-            chars_dict[char] = 0
-        chars_dict[char] += 1  
+        if char.isalpha():
+            if char not in chars_dict:
+                chars_dict[char] = 0  # creates a new key with a value of 0 
+            chars_dict[char] += 1  
     return chars_dict 
+
+def sort_dict(dict): # sorts by largest value
+    sorted_dict = sorted(dict.items(), key=lambda item: item[1], reverse=True)
+    return sorted_dict
 
 def main():
     PATH = "books/frankenstein.txt"
     with open(PATH) as f:
         file_contents = f.read()
-        print(char_count(file_contents))
+        dict = char_count(file_contents)
+        print(dict)
+        print(sort_dict(dict))
 main()
